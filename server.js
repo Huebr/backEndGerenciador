4473a3ba22f3cc0d23d12b417ddad45c25e16e0b
HueBr <pedro.jorge@aluno.uece.br>
2018-02-14 13:02:01 -0300

    created basic api.

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var auth = require('./controller/auth')();
+var index = require('./controller/index');
+var api = require('./controller/api');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+app.use(express.static('static'));
+
+app.use(auth.initialize());//protected
+
+app.use("/",index);
+app.use("/api/",auth.authenticate(),api);
+
+app.listen(3000,()=>{
+    console.log('App started port 3000.');
+});
